96d889f14b1af6ec7f1aebb6049a68360264ef32
Aashish Bansal <aashish22bansal@gmail.com>
2023-02-26 19:29:37 +0530

    Initial Code
    Initial Code

---
+++ Hue Rotation/Hue Rotation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hue Rotation
+//
+//  Created by Aashish Bansal on 26/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hue Rotation/Hue Rotation/Hue_RotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_RotationApp.swift
+//  Hue Rotation
+//
+//  Created by Aashish Bansal on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Hue_RotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
