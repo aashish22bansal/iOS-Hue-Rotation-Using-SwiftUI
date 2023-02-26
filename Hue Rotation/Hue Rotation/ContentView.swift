bf311f0a95c7b885656b889a2fb2d8f28809ba38
Aashish Bansal <aashish22bansal@gmail.com>
2023-02-27 01:22:08 +0530

    Initial Code and Resources
    Initial Code with the Images used in the Project.

--- Hue Rotation/Hue Rotation/ContentView.swift
+++ Hue Rotation/Hue Rotation/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Adding the Animation Properties
+    @State private var shiftColors = false
+    
+    // Propertry to track the Images dropped into the Asset Catalog
+    @State private var image = "dog"
+    
+    // Adding the background Property
+    let backgroundColor = Color(.black)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack{
+                // Setting the Background Color
+                backgroundColor
+                    .edgesIgnoringSafeArea(.all)
+                    .scaleEffect(1.4)
+                
+                // Adding the Image
+                Image(image)
+                    .resizable()
+                    .padding(20)
+                    .frame(width: 350, height: 350)
+                    .hueRotation(.degrees(shiftColors ? 360*2:0)) // Animating the Colors in the Image using Hue Rotation
+                    .animation(Animation.easeInOut(duration: 2).delay(0.4).repeatForever(autoreverses: true)) // Adding the Animation on this
+                    .onAppear(){ // starting things
+                        shiftColors.toggle()
+                    }
+            }
+            // Calling the ImagePicker with the selected Image
+            ImagePicker(selectedImage: $image)
+            .frame(width: 350, height: 200)
+        }.background(Color.black)
+            .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+// Image Picker struct to pick images from the Asset Catalog
+struct ImagePicker: View{
+    // Binding to the ContentView
+    @Binding var selectedImage: String
+    
+    // Adding the Array of Images
+    let images: [String] = ["ornament","landscape","dog","dice","cat"]
+    
+    var body: some View{
+        // Adding a Picker that is wheel style
+        Picker("", selection: $selectedImage){
+            // Looping over the Images
+            ForEach(images, id: \.self){ value in
+                Text(value)
+                    .foregroundColor(.white)
+            }.pickerStyle(RadioGroupPickerStyle()) // Setting the Picker style to wheel
+                .frame(width: 300, height: 150)
+                .background(Color.red.colorMultiply(.blue))
+                .cornerRadius(20)
+                .shadow(color: .white, radius: 5, x: 0, y: 0)
         }
-        .padding()
     }
 }
 
